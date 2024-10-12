989b1c02fc7157cbfecfd7ccf9b8ce7856c83b99
Cristóbal Ramos <cristobalramosmerino@gmail.com>
2024-10-12 13:29:05 -0300

    first alpha version layout

--- eleventy.config.js
+++ eleventy.config.js
@@ -6,17 +6,8 @@ export default function (eleventyConfig) {
 	eleventyConfig.setInputDirectory("_input");
 	eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
 	eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
-		// which file extensions to process
 		extensions: "html",
-
-		// Add any other Image utility options here:
-
-		// optional, output image formats
 		formats: ["auto"],
-		// optional, output image widths
-		// widths: ["auto"],
-
-		// optional, attributes assigned on <img> override these values.
 		defaultAttributes: {
 			loading: "lazy",
 			decoding: "async",
