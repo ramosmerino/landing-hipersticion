5fc12f4c83390b62af713fc95465c3eaa341f685
Cristóbal Ramos Merino <cristobalramosmerino@gmail.com>
2025-10-27 01:39:49 -0300

    feat: better marquee

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,18 +2,26 @@ import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
 
 const blog = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
 	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-	// Type-check frontmatter using a schema
 	schema: ({ image }) =>
 		z.object({
 			title: z.string(),
 			description: z.string(),
-			// Transform string to Date object
 			pubDate: z.coerce.date(),
 			updatedDate: z.coerce.date().optional(),
 			heroImage: image().optional(),
 		}),
 });
 
-export const collections = { blog };
+const footer = defineCollection({
+	loader: glob({ base: './src/content/footer', pattern: '**/*.{md,mdx}' }),
+	schema: z.object({
+		marqueeLinks: z.array(z.object({
+			text: z.string(),
+			url: z.string(),
+		})).optional(),
+		marqueeText: z.string().optional(),
+	}),
+});
+
+export const collections = { blog, footer };
